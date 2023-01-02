34b63fc83297201cfc090923c43042e2a437b8c4
Anjali Maheshwari <80675000+anjalim28@users.noreply.github.com>
2023-01-02 13:16:27 +0530

    Add files via upload

---
+++ day14.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(word[i]>='A' && word[i]<='Z')
+                count++;
+        }
+        if(count==0 || count == word.length() || (count==1 && word[0]>='A'&& word[0]<='Z'))
+        {
+            return true;
+        }
+        else
+        {
+        return false;
+        }
+    }
+};
+int main(){
+    Solution b;
+    cout<<b.detectCapitalUse("ffffffffffF");
+    
+    return 0;
+}
