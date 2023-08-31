015c284f96a48bc08b5a9c1086e6a5b8391e0b16
anjalim28 <anjalimaheshwari211@gmail.com>
2023-08-31 11:42:36 +0530

    day 255

--- august 2023/aug_22.cpp
+++ august 2023/aug_22.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    
-    return 0;
-}
+class Solution
+{
+public:
+    string convertToTitle(int columnNumber)
+    {
+        string s = "";
+        while (columnNumber > 0)
+        {
+            columnNumber--;
+            s = (char)(columnNumber % 26 + 'A') + s;
+            columnNumber /= 26;
+        }
+        return s;
+    }
+};
