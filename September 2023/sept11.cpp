22cf0aba04d019f3e9df54bd3eeee56d6ed5755e
anjalim28 <anjalimaheshwari211@gmail.com>
2023-09-13 14:29:34 +0530

    day268

--- September 2023/sept11.cpp
+++ September 2023/sept11.cpp
@@ -1,15 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
-int fib(int n)
-{
-	if (n <= 1)
-		return n;
-	return fib(n - 1) + fib(n - 2);
-}
-int main()
-{
-	int n = 9;
-	cout << fib(n);
-	// getchar();
-	return 0;
-}
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int, vector<int>> temp_group;
+        vector<vector<int>> result;
+        
+        for(int i = 0; i < groupSizes.size(); ++i) {
+            int size = groupSizes[i];
+            temp_group[size].push_back(i);
+            
+            if(temp_group[size].size() == size) {
+                result.push_back(temp_group[size]);
+                temp_group[size].clear();
+            }
+        }
+        return result;
+    }
+};
