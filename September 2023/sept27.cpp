130ba04a5e5e81f1dbbe800e1ad1b54eec783d93
anjalim28 <anjalimaheshwari211@gmail.com>
2023-09-28 19:30:21 +0530

    day283

---
+++ September 2023/sept27.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    string decodeAtIndex(string s, int k)
+    {
+        long long len = 0;
+        int i = 0;
+
+        while (len < k)
+        {
+            if (isdigit(s[i]))
+            {
+                len *= s[i] - '0';
+            }
+            else
+            {
+                len++;
+            }
+            i++;
+        }
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (isdigit(s[j]))
+            {
+                len /= s[j] - '0';
+                k %= len;
+            }
+            else
+            {
+                if (k == 0 || k == len)
+                {
+                    return string(1, s[j]); 
+                }
+                len--;
+            }
+        }
+
+        return ""; 
+    }
+};
