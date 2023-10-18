b2e28b2b6f71a2c1a7a934716bb3d43491c02f97
anjalim28 <anjalimaheshwari211@gmail.com>
2023-10-18 12:31:26 +0530

    day 303

---
+++ October 2023/oct17.cpp
@@ -0,0 +1,65 @@
+class Solution
+{
+public:
+    bool validateBinaryTreeNodes(int n, vector<int> &leftChild, vector<int> &rightChild)
+    {
+        unordered_map<int, vector<int>> graph;
+        vector<int> in_degree(n, 0);
+
+        for (int node = 0; node < n; ++node)
+        {
+            int left = leftChild[node];
+            int right = rightChild[node];
+            if (left != -1)
+            {
+                graph[node].push_back(left);
+                in_degree[left]++;
+            }
+            if (right != -1)
+            {
+                graph[node].push_back(right);
+                in_degree[right]++;
+            }
+        }
+
+        vector<int> root_candidates;
+        for (int node = 0; node < n; ++node)
+        {
+            if (in_degree[node] == 0)
+            {
+                root_candidates.push_back(node);
+            }
+        }
+
+        if (root_candidates.size() != 1)
+        {
+            return false;
+        }
+        int root = root_candidates[0];
+
+        queue<int> q;
+        unordered_set<int> seen;
+        q.push(root);
+        seen.insert(root);
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            if (graph.find(node) != graph.end())
+            {
+                for (int child : graph[node])
+                {
+                    if (seen.find(child) != seen.end())
+                    {
+                        return false;
+                    }
+                    seen.insert(child);
+                    q.push(child);
+                }
+            }
+        }
+
+        return seen.size() == n;
+    }
+};
