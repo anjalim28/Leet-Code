a193c3968100ca03c399192b8ef505efaa278707
anjalim28 <anjalimaheshwari211@gmail.com>
2024-02-06 21:43:31 +0530

    day 414

---
+++ February 2024/feb6.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    vector<vector<string>> groupAnagrams(vector<string> &words)
+    {
+        unordered_map<string, vector<string>> anagramGroups;
+
+        for (const string &word : words)
+        {
+            string key = word;
+            sort(key.begin(), key.end());
+
+            if (anagramGroups.find(key) == anagramGroups.end())
+            {
+                anagramGroups[key] = {word};
+            }
+            else
+            {
+                anagramGroups[key].push_back(word);
+            }
+        }
+
+        vector<vector<string>> result;
+        for (const auto &group : anagramGroups)
+        {
+            result.push_back(group.second);
+        }
+
+        return result;
+    }
+};
