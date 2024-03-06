ff8c1aa839034dedfaf1980e03e6ffd56fb909e3
anjalim28 <anjalimaheshwari211@gmail.com>
2024-03-06 19:55:54 +0530

    day 443

---
+++ March 2024/march6.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return false;
+        }
+        ListNode *fast = head, *slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
